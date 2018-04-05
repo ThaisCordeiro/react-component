f0b203ece80241a92725c5846f37a5e6277dc54d
ThaisCordeiro <thaiscrcordeiro@gmail.com>
2018-04-05 20:07:53 -0300

    create component OrderType

--- react-webhub/src/App.js
+++ react-webhub/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Card from './component/Card'
+import Card from './component/Card';
+import OrderType from './component/OrderType'
 
 class App extends Component {
   'use strict'
@@ -25,7 +25,7 @@ class App extends Component {
 
     return (
       <Card
-        id={ id }
+        key={ id }
         nome={ nome }
         sistema={ sistema_operacional }
         ano={ ano_lancamento }
@@ -34,16 +34,24 @@ class App extends Component {
     );
   }
 
+
+
+
+
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <OrderType/>
         </header>
 
-        { this.state.items.map(this.renderItem) }
+        <div className="container">
+          <div className="row">
+            { this.state.items.map(this.renderItem) }
+          </div>                  
+        </div>
+
 
       </div>
     );
--- react-webhub/src/component/Card.js
+++ react-webhub/src/component/Card.js
@@ -4,22 +4,24 @@ class Card extends Component {
   'use strict'
   render() {
     return (
-      <div className="card">
-        <figure className="card-image" 
-          style={{ backgroundImage:`url(${ this.props.image })` }}>
-        </figure>
+      <div className="col-md-3 col-sm-6">
+        <div className="card">
+          <figure className="card-image" 
+            style={{ backgroundImage:`url(${ this.props.image })` }}>
+          </figure>
 
-        <div className="card-body">
-          <h5 className="card-title">
-            { this.props.nome }
-          </h5>
+          <div className="card-body">
+            <h5 className="card-title">
+              { this.props.nome }
+            </h5>
 
-          <p>
-            <span>
-              Sistema Operacional/Ano:
-            </span>
-            { this.props.sistema }/{ this.props.ano }
-          </p>
+            <p>
+              <span>
+                Sistema Operacional/Ano:
+              </span>
+              { this.props.sistema }/{ this.props.ano }
+            </p>
+          </div>
         </div>
       </div>
     );
