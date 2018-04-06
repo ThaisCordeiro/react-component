5daefe230d118e337d21443c3bcd9ec1622977c9
ThaisCordeiro <thaiscrcordeiro@gmail.com>
2018-04-06 16:11:05 -0300

    feat: select order type

--- react-webhub/src/component/OrderType.js
+++ react-webhub/src/component/OrderType.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class OrderType extends Component {
+  'use strict'
+  constructor() {
+    super();
+    this.state = {
+      value: 'Selecione...',
+      reversed: '',
+      selected: true
+    }
+
+    this.itemChange = this.itemChange.bind(this);
+  }
+
+  
+
+  itemChange({ target }) {
+    this.setState({value: target.value})
+    this.props.itemSort( target.value );
+    // this.selected = true;
+    // this.reversed = false;
+  }
+ 
+ 
+  // itemReverse ({ target }) {
+  //   this.reversed = target.value;
+  //   this.selected = !target.value;
+  //   this.items = this.reversed ? this.items.reverse() : this.itemSort();
+  // }
+
+  
   render() {
     return (
-      <select>
-        <option defaultValue="" disabled selected>Selecione...</option>
-        <option defaultValue="nome">Nome</option>
-        <option defaultValue="sistema_operacional">Sistema Operacional</option>
-        <option defaultValue="ano_lancamento">Ano de Lançamento</option>
+      <select defaultValue={ this.state.value } onChange={ this.itemChange }>
+        <option defaultValue="" disabled>Selecione...</option>
+        <option value="nome">Nome</option>
+        <option value="sistema_operacional">Sistema Operacional</option>
+        <option value="ano_lancamento">Ano de Lançamento</option>
       </select>
     );
   }
